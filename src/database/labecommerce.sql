287e209fec858642a667cf8254034470e0128398
maumau1998 <kemillyleite85@gmail.com>
2023-01-27 22:57:17 -0300

    sql-parte-II

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -2,17 +2,18 @@
 
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 
 SELECT * FROM users;
 
-INSERT INTO users (id, name, email, password)
-VALUES ("u001", "Kemilly", "kemilly@gmail.com", "kemilly123"),
-("u002", "Cinara", "cinara@gmail.com", "cinara123"),
-("u003", "Mariana", "mariana@gmail.com", "mariana123");
+INSERT INTO users (id, email, password)
+VALUES ("u001", "kemilly@gmail.com", "kemilly123"),
+("u002", "cinara@gmail.com", "cinara123"),
+("u003", "mariana@gmail.com", "mariana123");
+
+DROP TABLE users;
 
 CREATE TABLE products (
    id TEXT PRIMARY KEY UNIQUE NOT NULL,
@@ -31,3 +32,59 @@ VALUES ("p001", "Coca-cola", 10, "Bebida"),
 ("p005", "feijão", 10, "Produto" );
 
 DROP TABLE products;
+
+--Aprofundamento SQL
+
+SELECT * FROM users;
+
+SELECT * FROM products;
+
+SELECT * FROM products
+WHERE name = "Birinight";
+
+INSERT INTO users (id, email, password)
+VALUES ("u004", "newUser@gmail.com", "newUser123");
+
+INSERT INTO products (id, name, price, category)
+VALUES ("p006", "Picanha", 65, "Produto");
+
+SELECT * FROM products
+WHERE id = "p006";
+
+SELECT * FROM users
+WHERE id = "u002";
+
+DELETE FROM users
+WHERE id = "u001";
+
+DELETE FROM products
+WHERE id = "p006";
+
+UPDATE users
+SET 
+     email = "matheus@gmail.com",
+     password = "matheus123"
+
+WHERE id = "u003";
+
+UPDATE products
+SET
+     name = "Abacaxi",
+     price = 5,
+     category = "Fruta"
+
+WHERE id = "p005";
+
+SELECT * FROM users
+ORDER BY email ASC;
+
+SELECT * FROM products
+ORDER BY price ASC
+LIMIT 20 OFFSET 0;
+
+SELECT * FROM products
+WHERE
+       price >= 300
+       AND price <= 1000
+
+ORDER BY price ASC;
